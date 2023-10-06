13b1152a2bc10a2b2b15aefd3f62bee346fc3b52
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-10-06 14:56:20 +0530

    Animated Border Card

--- app/src/main/java/com/ridesage/composepractice/AnimatedBorderCard.kt
+++ app/src/main/java/com/ridesage/composepractice/AnimatedBorderCard.kt
@@ -1,11 +1,83 @@
 package com.ridesage.composepractice
 
+import androidx.compose.animation.core.InfiniteTransition
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MovableContent
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.LinearGradient
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import java.time.Duration
 
 @Composable
 fun AnimatedBorderCard(
-
+    modifier: Modifier = Modifier,
+    shape: Shape = RoundedCornerShape(size = 0.dp),
+    borderWidth : Dp = 3.dp,
+    gradient: Brush = Brush.sweepGradient(listOf(Color.Gray,Color.White)),
+    animationDuration: Int = 10000,
+    onCardClick : () -> Unit = {},
+    content: @Composable () -> Unit
 )
 {
+    val infiniteTransition = rememberInfiniteTransition(label = "Infinite Color Transition")
+    val degrees by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = animationDuration, easing =  LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ),
+        label = "Infinite Colors"
+
+    )
+
+    Surface(
+        modifier = modifier
+            .clip(shape)
+            .clickable { onCardClick() },
+        shape = shape
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(borderWidth)
+                .drawWithContent {
+                    rotate(degrees = degrees) {
+                        drawCircle(
+                            brush = gradient,
+                            radius = size.width,
+                            blendMode = BlendMode.Src,
+                        )
+                    }
+                    drawContent()
+                },
+            color = MaterialTheme.colorScheme.surface,
+            shape = shape
+        ) {
+            content()
+        }
+    }
 
 }
--- app/src/main/java/com/ridesage/composepractice/MainActivity.kt
+++ app/src/main/java/com/ridesage/composepractice/MainActivity.kt
@@ -4,17 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ridesage.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,12 +32,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePracticeTheme {
+                Column(modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.surface),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                )
+                {
+                    AnimatedBorderCard(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(all = 24.dp),
+                        shape = RoundedCornerShape(8.dp),
+                        onCardClick = {},
+                        gradient = Brush.linearGradient(listOf(Color.Magenta, Color.Cyan))
+
+                    ){
+                        Column(modifier = Modifier.padding(all=24.dp)) {
+
+                            Text(text = "Ashutosh",
+                                fontWeight = FontWeight.Bold,
+                                fontSize = MaterialTheme.typography.displaySmall.fontSize)
+
+                            Spacer(modifier =Modifier.height(15.dp) )
+
+                            Text(text = "Hello EveryOne, My Name is Ashutosh Pandey. My Name is Ashutosh Pandey. My Name is Ashutosh Pandey. My Name is Ashutosh Pandey. My Name is Ashutosh Pandey",
+                                fontWeight = FontWeight.Bold,
+                                fontSize = MaterialTheme.typography.bodyMedium.fontSize)
+                        }
+
+
+                    }
+                }
 
             }
         }
     }
 }
-
 
 
 
