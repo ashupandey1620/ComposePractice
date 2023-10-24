b78ebd43b0b541abc3331cbbd48c0cc69729c2bb
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-10-24 08:35:34 +0530

    Animations in Jetpack Compose

--- app/src/main/java/com/ridesage/composepractice/AnimationImage.kt
+++ app/src/main/java/com/ridesage/composepractice/AnimationImage.kt
@@ -1,8 +1,21 @@
 package com.ridesage.composepractice
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 
 @Composable
 fun AnimationImage() {
+Column (modifier = Modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center){
 
+    Image(painter = painterResource(id = R.drawable.yellowcar), contentDescription = "Yellow Car" )
+
+}
 }
