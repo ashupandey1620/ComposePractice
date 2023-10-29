ae8a4506d72d53b9642fb496374128212befdd4e
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-10-29 16:17:17 +0530

    Auto Pager

--- app/src/main/java/com/ridesage/composepractice/AutoImageSlider.kt
+++ app/src/main/java/com/ridesage/composepractice/AutoImageSlider.kt
@@ -7,13 +7,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.pager.ExperimentalPagerApi
@@ -52,7 +62,8 @@ fun MyApplication(modifier: Modifier = Modifier) {
                     .wrapContentSize()
                     ) {currentPage->
 
-                Card(modifier
+                Card(
+                    modifier
                         .wrapContentSize()
                         .padding(26.dp),
                     elevation = CardDefaults.cardElevation(8.dp)
@@ -60,6 +71,32 @@ fun MyApplication(modifier: Modifier = Modifier) {
                 ) {
                     Image(painter = painterResource(id = images[currentPage]),contentDescription ="")
                 }
+
+            }
+
+            IconButton(onClick = {  },
+                 modifier.padding(30.dp)
+                     .size(48.dp)
+                     .align(Alignment.CenterEnd)
+                     .clip(CircleShape)) {
+
+                Icon(imageVector = Icons.Filled.KeyboardArrowLeft,contentDescription = "",
+                    modifier.fillMaxSize(),
+                    tint = Color.LightGray)
+
+
+            }
+
+            IconButton(onClick = {  },
+                modifier.padding(30.dp)
+                    .size(48.dp)
+                    .align(Alignment.CenterStart)
+                    .clip(CircleShape)) {
+
+                Icon(imageVector = Icons.Filled.KeyboardArrowRight,contentDescription = "",
+                    modifier.fillMaxSize(),
+                    tint = Color.LightGray)
+
 
             }
         }
