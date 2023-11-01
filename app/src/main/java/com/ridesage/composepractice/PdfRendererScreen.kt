424e9be44d7c24bab4aafdb7809eaa3425c83326
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-11-01 19:16:22 +0530

    Speakable Text

---
+++ app/src/main/java/com/ridesage/composepractice/PdfRendererScreen.kt
@@ -0,0 +1,9 @@
+package com.ridesage.composepractice
+
+import android.graphics.pdf.PdfRenderer
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PdfRendererScreen() {
+
+}
---
+++ app/src/main/java/com/ridesage/composepractice/PdfRendererViewModel.java
@@ -0,0 +1,4 @@
+package com.ridesage.composepractice;
+
+public class PdfRendererViewModel {
+}
