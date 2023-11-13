3323d6fb7d477eb0c15021e5504c84b2bddc4d8b
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-11-13 22:37:20 +0530

    Animate As State

--- app/src/main/java/com/ridesage/composepractice/AnimationPractice.kt
+++ app/src/main/java/com/ridesage/composepractice/AnimationPractice.kt
@@ -1,11 +1,19 @@
 package com.ridesage.composepractice
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,7 +21,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import kotlin.reflect.KProperty
 
 @Composable
@@ -25,20 +35,44 @@ fun AnimationPractice() {
         var isVisible by remember {
             mutableStateOf(false)
         }
+
+        var isRound by remember {
+            mutableStateOf(false)
+        }
         
         androidx.compose.material3.Button(onClick = {
             isVisible = !isVisible
+            isRound = !isRound
         }) {
             Text(text = "Toggle")
         }
 
-        AnimatedVisibility(visible = isVisible,
-            modifier = Modifier.fillMaxWidth().weight(1f)) {
+        val borderRadius by animateIntAsState(
+            targetValue = if(isRound) 100 else 0,
+            animationSpec = tween(
+                durationMillis = 2000,
+                delayMillis = 500
+            )
+        )
 
-            Box (modifier = Modifier.background(Color.Red)){
 
-            }
+        Box (modifier = Modifier.size(200.dp)
+            .clip(RoundedCornerShape(borderRadius))
+            .background(Color.Red)){
+
         }
+//        AnimatedVisibility(visible = isVisible,
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .weight(1f),
+//            enter = slideInHorizontally() + fadeIn(),
+//            exit  = slideOutHorizontally() + fadeOut()
+//        ) {
+//
+//            Box (modifier = Modifier.background(Color.Red)){
+//
+//            }
+//        }
     }
 }
 
